d914d805b5b3b497ec8771aeefe47f2789bd3876
kun2256 <2711842844@qq.com>
2023-05-05 12:51:09 +0800

    fead

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -16,18 +16,18 @@ map("n", "sc", "<C-w>c", opt)
 -- 关闭其他
 map("n", "so", "<C-w>o", opt)
 -- Alt + hjkl  窗口之间跳转
-map("n", "<A-h>", "<C-w>h", opt)
-map("n", "<A-j>", "<C-w>j", opt)
-map("n", "<A-k>", "<C-w>k", opt)
-map("n", "<A-l>", "<C-w>l", opt)
+map("n", "sh", "<C-w>h", opt)
+map("n", "sj", "<C-w>j", opt)
+map("n", "sk", "<C-w>k", opt)
+map("n", "sl", "<C-w>l", opt)
 -- 左右比例控制
 map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
 map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
 map("n", "s,", ":vertical resize -20<CR>", opt)
 map("n", "s.", ":vertical resize +20<CR>", opt)
 -- 上下比例
-map("n", "sj", ":resize +10<CR>", opt)
-map("n", "sk", ":resize -10<CR>", opt)
+-- map("n", "sj", ":resize +10<CR>", opt)
+-- map("n", "sk", ":resize -10<CR>", opt)
 map("n", "<C-Down>", ":resize +2<CR>", opt)
 map("n", "<C-Up>", ":resize -2<CR>", opt)
 -- 等比例
