b5fc620c11c494bba20b078172b8d83cdf3488a9
欧阳坤 <2711842844@qq.com>
2025-04-30 11:27:59 +0800

    【debug】修改nvim-tree快捷键接口bug

--- lua/gwoo/nvim-tree.lua
+++ lua/gwoo/nvim-tree.lua
@@ -5,8 +5,39 @@ if not status then
 end
 
 -- 列表操作快捷键
-local list_keys = require('keybindings').nvimTreeList
+
+local function my_on_attach(bufnr)
+    local api = require("nvim-tree.api")
+    
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    
+    local map = vim.keymap.set
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+    
+    -- custom mappings
+    map('n', '<CR>', api.node.open.edit, opts('Open'))
+    map('n', 'o', api.node.open.edit, opts('Open'))
+    map('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+    map('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+    map('n', 'h', api.node.open.horizontal, opts('Open: Horizontal Split'))
+    map('n', 'i', api.tree.toggle_custom_filter, opts('Toggle Filter: Hidden'))
+    map('n', '.', api.tree.toggle_hidden_filter, opts('Toggle Filter: Dotfiles'))
+    map('n', 'R', api.tree.reload, opts('Refresh'))
+    map('n', 'a', api.fs.create, opts('Create File Or Directory'))
+    map('n', 'd', api.fs.remove, opts('Delete'))
+    map('n', 'r', api.fs.rename, opts('Rename'))
+    map('n', 'x', api.fs.cut, opts('Cut'))
+    map('n', 'c', api.fs.copy.node, opts('Copy'))
+    map('n', 'p', api.fs.paste, opts('Paste'))
+    map('n', '<C-j>', api.tree.change_root_to_parent, opts('Up'))
+    map('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
 nvim_tree.setup({
+    on_attach = my_on_attach,
     -- 不显示 git 状态图标
     git = {
         enable = false,
@@ -29,11 +60,6 @@ nvim_tree.setup({
         side = 'left',
         -- 隐藏根目录
 --         hide_root_folder = false,
-        -- 自定义列表中快捷键
---         mappings = {
---             custom_only = false,
---             list = list_keys,
---         },
         -- 不显示行数
         number = false,
         relativenumber = false,
@@ -47,11 +73,6 @@ nvim_tree.setup({
             -- 打开文件时关闭
             quit_on_open = true,
         },
-    },
-    -- wsl install -g wsl-open
-    -- https://github.com/4U6U57/wsl-open/
-    system_open = {
-        cmd = 'wsl-open', -- mac 直接设置为 open
     },
     -- project plugin 需要这样设置
     update_cwd = true,
--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -71,26 +71,6 @@ local pluginKeys = {}
 -- nvim-tree
 -- alt + m 键打开关闭tree
 map("n", "tr", ":NvimTreeToggle<CR>", opt)
--- 列表快捷键
-pluginKeys.nvimTreeList = {
-    -- 打开文件或文件夹
-    { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
-    -- 分屏打开文件
-    { key = "v", action = "vsplit" },
-    { key = "h", action = "split" },
-    -- 显示隐藏文件
-    { key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
-    { key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
-    -- 文件操作
-    { key = "<F5>", action = "refresh" },
-    { key = "a", action = "create" },
-    { key = "d", action = "remove" },
-    { key = "r", action = "rename" },
-    { key = "x", action = "cut" },
-    { key = "c", action = "copy" },
-    { key = "p", action = "paste" },
-    { key = "s", action = "system_open" },
-}
 
 -- 切换 buff 栏
 map("n", "B", ":BufferLinePick<cr>", opt)
@@ -98,8 +78,8 @@ map("n", "B", ":BufferLinePick<cr>", opt)
 map("n", "cb", ":BufferLinePickClose<cr>", opt)
 map("n", "<C-w>", ":Bdelete!<CR>", opt)
 -- 左右Tab切换
-map("n", "<A-h>", ":BufferLineCyclePrev<CR>", opt)
-map("n", "<A-l>", ":BufferLineCycleNext<CR>", opt)
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
 
 -- Telescope
 -- 查找文件
